14c2fffea97c549d261807fce9a43b63387ac0dd
Luis Raul Espinoza Barboza <luis.espinoza@buildbinder.com>
2017-07-19 15:09:55 -0600

    added login and register viewmodels

---
+++ Base/Models/Identity/RegisterViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Base.Models.Identity
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name ="Email")]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm")]
+        [Compare("Password",ErrorMessage ="The password and confirmation don't match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
