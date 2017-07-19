e9a8b255af67b01daaedb0e44b8af971f34dc2e3
Luis Raul Espinoza Barboza <luis.espinoza@buildbinder.com>
2017-07-19 13:14:43 -0600

    configured mvc default route, controller and view

--- Base/Startup.cs
+++ Base/Startup.cs
@@ -16,6 +16,7 @@ namespace Base
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,9 +29,11 @@ namespace Base
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseMvcWithDefaultRoute();
+
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("You are in problems!");
             });
         }
     }
